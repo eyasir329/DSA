b27eaf8421c0fc52b8f41960a964c24cb2a3c291
Eyasir Ahamed <eyasir329@gmail.com>
2023-10-21 22:29:50 +0600

    Update cin_get.cpp

--- String/cin_get.cpp
+++ String/cin_get.cpp
@@ -6,6 +6,7 @@ int main(){
     char sentence[1000];
     //one character at a time but read everything including whitespace
     char temp = cin.get();
+    //getline(cin,str) can input all thing
     int len = 0;
     while(temp!='\n'){
         sentence[len++] = temp;
