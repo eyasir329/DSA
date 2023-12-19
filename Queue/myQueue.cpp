3c98d0c1829ee010e6af8600493370db678db04d
Eyasir Ahamed <eyasir329@gmail.com>
2023-12-19 18:34:29 +0600

    queue

---
+++ Queue/myQueue.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "Queue.h"
+
+using namespace std;
+
+int main(){
+    Queue myqueue(7);
+    myqueue.push(1);
+    myqueue.push(2);
+    myqueue.push(3);
+    myqueue.push(4);
+    myqueue.push(5);
+    myqueue.push(6);
+    myqueue.push(7);
+    myqueue.push(8);//out of ms
+    myqueue.pop();
+    myqueue.pop();
+    myqueue.push(9);
+
+    while (!myqueue.empty()){
+        cout<<myqueue.getFront()<<endl;
+        myqueue.pop();
+    }
+    return 0;
+}
